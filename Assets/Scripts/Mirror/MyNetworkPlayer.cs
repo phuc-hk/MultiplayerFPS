27ff3ce44f0c2f567fda4f430f0324fe1b564ab9
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-05-07 17:24:03 +0700

    Add remote actions

--- Assets/Scripts/Mirror/MyNetworkPlayer.cs
+++ Assets/Scripts/Mirror/MyNetworkPlayer.cs
@@ -8,7 +8,35 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SyncVar][SerializeField] string playerName = "Empty";
     [SyncVar][SerializeField] Color playColor = Color.white;
 
+    #region Server
+    [Server]
     public void SetPlayerName(string newName) { playerName = newName; }
 
+    [Server]
     public void SetPlayerColor(Color newColor) { playColor = newColor; }
+
+    [Command] private void SetNewName(string newName) 
+    {       
+        playerName = newName;
+        RcpSetNewName();
+    }
+
+    
+
+    #endregion
+
+    #region Client
+    [ContextMenu("Set New Name")]
+    private void SetNewName()
+    {
+        SetNewName("New Player Name");
+    }
+
+    [ClientRpc]
+    private void RcpSetNewName()
+    {
+        Debug.Log(playerName);
+    }
+
+    #endregion
 }
